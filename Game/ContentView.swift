7744a95826a5977c0c7db7ee6bec8248f7af908e
iMaxiOS <urk.maksim1@yandex.ru>
2020-05-29 16:12:05 +0300

    add project

---
+++ Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game
+//
+//  Created by Maxim Granchenko on 29.05.2020.
+//  Copyright © 2020 Maxim Granchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
